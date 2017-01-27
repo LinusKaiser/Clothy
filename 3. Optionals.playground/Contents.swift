f3ae93bf069d25c235b7d950619559255b38ce30
LinusKaiser <Linus_kaiser@outlook.de>
2017-01-27 15:02:06 +0100

    Test 1.0.0

---
+++ 3. Optionals.playground/Contents.swift
@@ -0,0 +1,45 @@
+//optionals
+
+var fehlerCode: Int?
+fehlerCode = nil
+
+//forced unwrapping
+
+if fehlerCode != nil{
+    print(fehlerCode!)
+}
+
+//implicit unwrapping
+
+var fehlerMeldung: String!
+fehlerMeldung = nil
+if fehlerMeldung != nil{
+    print(fehlerMeldung!)
+}
+
+//guard
+
+func addieren(x: Int?,y: Int?) -> Int?{
+    guard x != nil else{
+        return nil
+    }
+    guard y != nil else{
+        return nil
+    }
+    
+    return x! + y!
+    
+}
+
+// if let
+
+var alter: Int?
+alter = 19
+
+
+
+if let meinAlter = alter{
+    if let fehler = fehlerMeldung{
+        print(meinAlter)
+    }
+}
